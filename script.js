627af5c90872c6560f5e6e60de7b344c76631144
root <root@debian>
2024-01-16 22:12:27 +0100

    Ajout nouvel index

--- script.js
+++
@@ -1,8 +0,0 @@
-function show_sub(cat){
-	event.stopPropagation();
-	cat.querySelector("ul").style.display = (cat.querySelector("ul").style.display == "block") ? "none" : "block";
-}
-$("li:parent").attr("onclick", "show_sub(this)");
-$("body").on("click", function(){
-	$("nav ul").style.display = "none";
-});
